52c3ce9215f67595f5b4472fc3cb67785f181eec
Umur Dinç <umur.dinc@gmail.com>
2020-03-22 19:54:34 +0300

    Update and rename WRF4.1.4_Install.bash to WRF4.1.5_Install.bash

--- WRF4.1.5_Install.bash
+++ WRF4.1.5_Install.bash
@@ -2,10 +2,10 @@
 #########################################################
 #		WRF Install Script     			#
 # 	This Script was written by Umur Dinç    	#
-#  To execute this script "bash WRF4.1.4_Install.bash"	#
+#  To execute this script "bash WRF4.1.5_Install.bash"	#
 #########################################################
-echo "Welcome! This Script will install the WRF4.1.4"
-echo "Installation may take several hours and it takes 52 GB storage. Be sure that you have enough time and storage"
+echo "Welcome! This Script will install the WRF4.1.5"
+echo "Installation may take several hours and it takes 52 GB storage. Be sure that you have enough time and storage."
 #########################################################
 #	Controls					#
 #########################################################
@@ -22,6 +22,7 @@ if [ -n "$packagemanagement" ]; then
         echo "Operating system uses apt packagemanagement"
 else
         echo "Error! This script is written for the operating systems which uses apt packagemanagement. Please try this script with debian based operating systems, such as, Ubuntu, Linux Mint, Debian, Pardus etc."
+#Tested on Linux Mint 19.3
 exit
 fi
 #########################################################
@@ -81,12 +82,12 @@ cd ..
 #	WRF Installation		#
 #########################################
 cd ..
-[ -d "WRF-4.1.4" ] && mv WRF-4.1.4 WRF-4.1.4-old
-[ -d "v4.1.4.tar.gz" ] && mv v4.1.4.tar.gz v4.1.4.tar.gz-old
-wget https://github.com/wrf-model/WRF/archive/v4.1.4.tar.gz
-mv v4.1.4.tar.gz WRFV4.1.4.tar.gz
-tar -zxvf WRFV4.1.4.tar.gz
-cd WRF-4.1.4
+[ -d "WRF-4.1.5" ] && mv WRF-4.1.5 WRF-4.1.5-old
+[ -d "v4.1.5.tar.gz" ] && mv v4.1.5.tar.gz v4.1.5.tar.gz-old
+wget https://github.com/wrf-model/WRF/archive/v4.1.5.tar.gz
+mv v4.1.5.tar.gz WRFV4.1.5.tar.gz
+tar -zxvf WRFV4.1.5.tar.gz
+cd WRF-4.1.5
 sed -i 's#  export USENETCDF=$USENETCDF.*#  export USENETCDF="-lnetcdf"#' configure
 sed -i 's#  export USENETCDFF=$USENETCDFF.*#  export USENETCDFF="-lnetcdff"#' configure
 cd arch
@@ -119,7 +120,7 @@ sed -i '141s/.*/  $response = 3 ;/' Config.pl
 cd ..
 ./clean
 sed -i '122s/.*/    NETCDFF="-lnetcdff"/' configure
-sed -i '154s/.*/standard_wrf_dirs="WRF-4.1.4 WRF WRF-4.0.3 WRF-4.0.2 WRF-4.0.1 WRF-4.0 WRFV3"/' configure
+sed -i '154s/.*/standard_wrf_dirs="WRF-4.1.5 WRF WRF-4.0.3 WRF-4.0.2 WRF-4.0.1 WRF-4.0 WRFV3"/' configure
 ./configure
 logsave compile.log ./compile
 sed -i "s# geog_data_path.*# geog_data_path = '../WPS_GEOG/'#" namelist.wps
