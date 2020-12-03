e424b144a3827852373c1c9bb0d7f70b36256ee2
Umur Dinç <umur.dinc@gmail.com>
2020-12-03 18:54:26 +0300

    Update WRF4.2.1_Install.bash

--- WRF4.2.1_Install.bash
+++ WRF4.2.1_Install.bash
@@ -2,7 +2,7 @@
 #########################################################
 #		WRF Install Script     			#
 # 	This Script was written by Umur Dinç    	#
-#  To execute this script "bash WRF4.1.5_Install.bash"	#
+#  To execute this script "bash WRF4.2.1_Install.bash"	#
 #########################################################
 WRFversion="4.2.1"
 type="ARW"
